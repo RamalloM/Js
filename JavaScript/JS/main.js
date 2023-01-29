1c9ed3ee4e69970237498234478facf457caa352
RamalloM <112563182+RamalloM@users.noreply.github.com>
2023-01-29 10:42:46 -0300

    JS Primera entrega, final

--- JavaScript/JS/main.js
+++ JavaScript/JS/main.js
@@ -1,64 +1,68 @@
 
-let saludo = alert("Bienvenido a su Heladera Virtual");
-let menuInicio;
-function menu() {
-    do {
-        menuInicio = prompt("Indique el numero de la lista para continuar: \n  1- Ingresar nuevo ingrediente \n 2- Stock total \n 3- Recetario \n 4- Nueva receta");
-       
-        // Numero 1: Ingreso de nuevos ingredientes
+let valorInicial;
+let tasaDeInteres;
 
-        if (menuInicio === "1") {
-            let nuevoIngrediente = prompt("Ingrese ingrediente");
-            let cantidad = prompt("Ingrese cantidad");
-            let unidad = prompt ("ingrese unidad de medida");
-            function stock (nuevoIngrediente, cantidad, unidad){
-                let  stockTotal = stockTotal + "\n" + nuevoIngrediente + " " + cantidad + unidad;
-              }
-             
-            menu();
-
-        // Numero 2: Imprime el total del stock
-
-        } else if (menuInicio == "2") {
-            
-            
-
-
-
-
-        } else if (menuInicio === "3") {
-            alert('Su recetario actual es: ' + recetarioActual);
-
-        } else if (menuInicio === "4") {
-            let nuevaReceta = prompt("Ingrese nombre de receta");
-
-        } else {
-            alert("ingrese un numero valido");
-        }
-    } while (menuInicio >= 5 || menuInicio != Number);
+alert('Bienvenido a la tienda virtual de Pokemon');
+ 
+let compra = Number(prompt("Ingrese el codigo del valorInicial deseado: \n 1- Pokeball 2000$ \n 2- Ultraball 4000$ \n 3- Pocion 1500$ \n 4- Caramelo Raro 8000$ \n 5- Antiparalisis 1000$ \n 6- Despertar 800$"));
+while(compra < 1 || compra > 7)
+{
+    alert('Ingrese valor valido');
+    compra = Number(prompt("Ingrese el codigo del valorInicial deseado: \n 1- Pokeball 2000$ \n 2- Ultraball 4000$ \n 3- Pocion 1500$ \n 4- Caramelo Raro 8000$ \n 5- Antiparalisis 1000$ \n 6- Despertar 800$"));
+}
+switch (compra) {
+    case 1:
+        valorInicial = 2000;
+        break;
+    case 2:
+        valorInicial = 4000;
+        break;
+    case 3:
+        valorInicial = 1500;
+        break;
+    case 4:
+        valorInicial = 8000;
+        break;
+    case 5:
+        valorInicial = 1000;
+        break;
+    case 6:
+        valorInicial = 800;
+        break;
+    }
+  
+let cuotas = Number(prompt("En cuantas cuotas desea realizar su pago? \n Opciones de Pagos en Cuotas: \n 1 cuota sin interes \n 3 cuotas con 22% de interes \n 6 cuotas con 30% de interes \n 9 cuotas con 45% de interes \n 12 cuotas con 55% de interes"));
+while(cuotas != 1 && cuotas != 3 && cuotas !=6 && cuotas != 12)
+{
+    let cuotas = Number(prompt("En cuantas cuotas desea realizar su pago? \n Opciones de Pagos en Cuotas: \n 1 cuota sin interes \n 3 cuotas con 22% de interes \n 6 cuotas con 30% de interes \n 9 cuotas con 45% de interes \n 12 cuotas con 55% de interes"));
 }
 
-menu();
+if (cuotas === 1) {
+    tasaDeInteres = 0;
+} else if (cuotas === 3) {
+    tasaDeInteres = 0.22;
+} else if (cuotas === 6) {
+    tasaDeInteres = 0.30;
+} else if ((cuotas === 9)) {
+    tasaDeInteres = 0.45;
+} else if (cuotas === 12) {
+    tasaDeInteres = 0.55;
+}
+
+function valorTotal(x) {
+    let valorFinal = x + (x * tasaDeInteres);
+    return valorFinal;
+}
+function precioEnCuotas(x, y) {
+    let cuotaMensual = (x / y);
+    return Math.round(cuotaMensual);
+}
+let valorFinal = valorTotal(valorInicial);
 
 
-
-
-
-// let stockTotal = nuevoIngrediente + stockTotal;
-
-
-
-
-/* Heladera Virtual:
-
-1- Lugar donde almacenar los ingredientes disponibles.
-(Utilizar prompt para pedir el ingreso de ingredientes)
-
-Crear una funcion que permita almacenar el nuevoIngrediente y la cantidad en la lista del stock total.
-
-
-
-
-2- Cotejar ingredientes dados con recetas ya almacenadas en el programa.
-(promt con if, para ingresar ingredientes o recetas).
-*/
+if(cuotas === 1){
+alert('El valor total de su compra es de ' + valorFinal + " $" + ' a pagar en ' + cuotas + ' cuota' + ' de ' + precioEnCuotas(valorFinal, cuotas) + ' $.');
+}
+else{
+    alert('El valor total de su compra es de ' + valorFinal + " $" + ' a pagar en ' + cuotas + ' cuotas' + ' de ' + precioEnCuotas(valorFinal, cuotas) + ' $.');
+}
