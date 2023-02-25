89961495af7290a3d5320a7fdf0959393a708339
RamalloM <112563182+RamalloM@users.noreply.github.com>
2023-02-25 12:12:46 -0300

    js

--- JavaScript/JS/main.js
+++ JavaScript/JS/main.js
@@ -1,10 +1,9 @@
-
 //Funcion constructora
 class Ingredientes {
     constructor(producto, cantidad, unidad) {
-        this.producto = (producto);
-        this.cantidad = (cantidad);
-        this.unidad = (unidad);
+        this.producto = producto.toLowerCase();
+        this.cantidad = cantidad;
+        this.unidad = unidad;
     }
 }
 
@@ -13,13 +12,116 @@ const fideos = new Ingredientes('fideos', '2', 'paq.');
 const cebolla = new Ingredientes('cebolla', '6', 'u.');
 
 //Array de objetos
-const stockDeIngredientes = [fideos, cebolla];
+const stockDeProductos = [fideos, cebolla];
+
+
+const primalContainer = document.getElementById("primalContainer");
+
+const div = document.createElement("div");
+
+    div.innerHTML =`
+    <div id="ingresoDeIngredientes>
+    <form id="formulario">
+        <fieldset>
+        <label for="producto">Ingrese ingrediente aqui</label>
+        <input type="text" id="producto"></input>
+            <br>
+        <label for="cantidad">Ingrese cantidad aqui</label>
+        <input type="text" id="cantidad"></input>
+            <br>
+        <label for="unidad">Ingrese la unidad de medida deseada</label>
+        <input type="text" id="unidad"></input> 
+            <br>
+        <button id="btnAdd"> Guardar ingrediente </button> 
+    </fieldset>
+    </form>
+    </div>      
+                        
+    <div id="carrito">
+            <h2> Lista de Ingredientes </h2>
+    </div>
+
+                    `;
+
+primalContainer.appendChild(div);
+
+const carrito = document.getElementById("carrito");
+carrito.className ="carrito";
+
+const formulario = document.getElementById("formulario");
+
+//modificar funcionIngresoDeIngredientes para que al hacer cick guarde el producto en carrito.
+formulario.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const producto = document.getElementById("producto");
+    const cantidad = document.getElementById("cantidad");
+    const unidad = document.getElementById("unidad");
+    const productoIngresado = new Ingredientes(producto.value, cantidad.value, unidad.value);
+    stockDeProductos.push(productoIngresado);
+    console.log(stockDeProductos);
+    formulario.reset();
+}
+);
+
+
+
+
+
+//Comentarios
+
+
+
+
+
+//Ingresar buscador de recetas 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Ejecucion del programa
+
+//programa();
+
+
+
+
+
+
+
+/* function decrementoDeIngredientes() {
+    let productoAEliminar = prompt('Indique el nombre del producto a eliminar');
+    // Anuncio: ustede posee x elementos, cuantos desea eliminar?
+    let cantidadAElimnar = parseInt(prompt("Ingrese cantidad a eliminar"));
+    let ingrediente = stockDeIngredientes.find(producto => producto.producto === productoAEliminar);
+    let indice = stockDeIngredientes.indexOf(ingrediente);
+
+    stockDeIngredientes.splice(indice, 1);
+    console.log(stockDeIngredientes);
+}
+ */
+
+
+
+
+
 
 // Funciones 
 
-let flag = true;
+/* let flag = true; */
 
-function ingresoDeIngredientes() {
+
+// Ingresar ingredientes CHECK!!
+/* function ingresoDeIngredientes() {
     let producto = prompt('Ingrese el nombre del producto');
     let cantidad = parseInt(prompt('Indique cantidad del producto a ingresar'));
     let unidad = prompt("Ingrese una unidad del listado: gr, cc, u.");
@@ -31,11 +133,13 @@ function ingresoDeIngredientes() {
     }
     else if (condicion === 'N' || condicion === "n") {
         console.log(stockDeIngredientes);
+
+
     }
 }
 
 
-
+// Egreso de ingredientes (Solo se puede eliminar un objeto.)
 function egresoDeIngredientes() {
     let productoAEliminar = prompt('Indique el nombre del producto a eliminar');
     let ingrediente = stockDeIngredientes.find(producto => producto.producto === productoAEliminar);
@@ -44,6 +148,11 @@ function egresoDeIngredientes() {
     console.log(stockDeIngredientes);
 }
 
+
+
+// Falta funcion para decremetnear cantidad de objetos!!!!
+
+// Mostrar stock y terminar el programa CHECK
 function mostrarStock() {
     console.log(stockDeIngredientes);
 }
@@ -52,6 +161,9 @@ function salir() {
     flag = false;
     alert("Gracias por usar su Alacena Virtual");
 }
+
+
+//Funcion auxiliar para actualizar elementos.
 
 function inProgress() {
     alert("Esta funcion aun no se encuentra disponible, lo sentimos")
@@ -62,7 +174,7 @@ function inProgress() {
 
 function programa() {
     alert("Bienvenido a su Alacena Virtual");
-    
+
     while (flag) {
         let opcionInicial = parseInt(prompt("Indique el numero de la lista para continuar: \n  1) Ingresar nuevo ingrediente \n 2) Stock total \n 3) Eliminar ingrediente \n 4) Recetario \n 5) Salir"));
         switch (opcionInicial) {
@@ -83,13 +195,18 @@ function programa() {
         }
     }
 }
-
-//Ejecucion del programa
-
-programa();
+ */
 
 
- 
+
+
+
+
+
+
+
+
+
 
 
 
